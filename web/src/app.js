6069f32918bc7f4a07676d30fd7af378b21ff5c4
Bianka <email@email.hu>
2025-05-21 12:17:57 +0200

    index.html és app.js első változat kész

---
+++ web/src/app.js
@@ -0,0 +1,45 @@
+/*
+* File: app.js
+* Author: Nagy Bianka
+* Copyright: 2025, Nagy Bianka
+* Group: Szoft I-N
+* Date: 2025-05-21
+* Github: https://github.com/Bianka-9/
+*/
+
+const tbody = document.querySelector('#tbody');
+const url = 'http://localhost:8000/api/bikes'
+var bikeList = []
+function getBikes(){
+    fetch(url)
+    .then(response=>response.json())
+    .then(result =>{
+        console.log(result.data)
+        bikeList = result.data
+        renderTbody()
+    })
+
+}
+
+
+
+function renderTbody(){
+    let rows =""
+    for(let bike of bikeList){
+        
+        rows +=
+        `
+        <tr>
+            <td>${bike.id}</td>
+            <td>${bike.name}</td>
+            <td>${bike.wheel}</td>
+            <td>${bike.usage}</td>
+            <td>${bike.price}</td>
+        </tr>
+        `
+    }
+    console.log(rows)
+    tbody.innerHTML = rows;
+}
+
+getBikes()
